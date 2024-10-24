0731c4cffb04953f3054e66d08dedfde5b192b1a
ideftbuild <a.akanSWE@gamil.com>
2024-10-24 05:23:25 +0100

    feat: Implement `get`, `get_str` and `get_int` methods

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """Module: exercise"""
 import redis
 from uuid import uuid4
-from typing import Union
+from typing import Union, Callable
 
 
 class Cache:
@@ -24,3 +24,23 @@ class Cache:
         key: str = str(uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Callable = None) \
+            -> Union[str, bytes, int, float]:
+        """
+        Retrieve data from the Redis database.
+
+        :param key: The key under which the data is stored.
+        :param fn: An optional conversion function to apply to the data.
+        :return: The data stored at the specified key,
+                optionally converted using the provided function.
+        """
+        return self._redis.get(fn(key) if fn else key)
+
+    def get_str(self, key: str) -> str:
+        """Retrieve a string from the Redis database."""
+        return self.get(key, str)
+
+    def get_int(self, key: str) -> int:
+        """Retrieve an integer from the Redis database."""
+        return self.get(key, int)
